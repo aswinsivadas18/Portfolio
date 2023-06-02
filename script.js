29e07eadf2d092da74f72624eb1a399b933ba4bc
aswinsivadas18 <aswinsivadas18@gmail.com>
2023-06-02 12:31:51 +0530

    Portfolio
    new

--- script.js
+++ script.js
@@ -39,14 +39,14 @@ $(document).ready(function () {
 
   // Typing animation script start
   var typed = new Typed(".typing", {
-    strings: ["React.js Developer", "Front End Developer", "Youtuber", "Teacher"],
+    strings: ["Full Stack Developer", "React.Js Developer", "Photographer", "Mechanical Engineer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing2", {
-    strings: ["React.js Developer", "Front End Developer", "Youtuber", "Teacher"],
+    strings: ["Full Stack Developer", "React.Js Developer", "Photographer", "Mechanical Engineer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
